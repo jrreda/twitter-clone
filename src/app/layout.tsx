e652238e0d9c689598b00cc14ee2a5640cfa5b73
mreda <mreda@ntgclarity.com>
2025-07-16 10:47:09 +0300

    LeftBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
       <body>
         <div className="flex justify-between max-w-screen-md mx-auto lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl">
           <div className="h-screen px-2 xsm:px-4 xxl:px-8"><LeftBar /></div>
-          <div className="h-screen lg:min-w-[600px] border-x-[1px] border-gray-100 flex-1">{children}</div>
+          <div className="h-screen lg:min-w-[600px] border-x-[1px] border-borderGray flex-1">{children}</div>
           <div className="hidden h-screen ml-4 md-ml-8 xl:flex flex-1"><RightBar /></div>
         </div>
       </body>
--- src/components/layout/LeftBar.tsx
+++ src/components/layout/LeftBar.tsx
@@ -1,9 +1,116 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+
+const menuList = [
+  {
+    id: 1,
+    name: "Homepage",
+    link: "/",
+    icon: "home.svg",
+  },
+  {
+    id: 2,
+    name: "Explore",
+    link: "/",
+    icon: "explore.svg",
+  },
+  {
+    id: 3,
+    name: "Notification",
+    link: "/",
+    icon: "notification.svg",
+  },
+  {
+    id: 4,
+    name: "Messages",
+    link: "/",
+    icon: "message.svg",
+  },
+  {
+    id: 5,
+    name: "Bookmarks",
+    link: "/",
+    icon: "bookmark.svg",
+  },
+  {
+    id: 6,
+    name: "Jobs",
+    link: "/",
+    icon: "job.svg",
+  },
+  {
+    id: 7,
+    name: "Communities",
+    link: "/",
+    icon: "community.svg",
+  },
+  {
+    id: 8,
+    name: "Premium",
+    link: "/",
+    icon: "logo.svg",
+  },
+  {
+    id: 9,
+    name: "Profile",
+    link: "/",
+    icon: "profile.svg",
+  },
+  {
+    id: 10,
+    name: "More",
+    link: "/",
+    icon: "more.svg",
+  },
+];
 
 export default function LeftBar() {
   return (
-    <div>
-      <h1>Left Bar</h1>
+    <div className='h-screen flex flex-col justify-between pt-2 pb-8 sticky top-0'>
+      {/* LOGO MENU BUTTONS */}
+      <div className='flex flex-col gap-4 items-center xxl:items-start'>
+        {/* LOGO */}
+        <Link href="/" className='p-2 rounded-full hover:bg-[#181818]'>
+          <Image src='icons/logo.svg' width={24} height={24} alt='logo' />
+        </Link>
+
+        {/* MENU LIST */}
+        <div className="flex flex-col gap-4">
+          {menuList.map((item) => (
+            <Link key={item.id} href={item.link} className='flex items-center gap-4 rounded-full p-2 hover:bg-[#181818]'>
+              <Image src={`icons/${item.icon}`} width={24} height={24} alt={item.name} />
+              <span className='hidden xxl:inline'>{item.name}</span>
+            </Link>
+          ))}
+        </div>
+
+        {/* BUTTONS */}
+        <Link href={'/'} className='bg-white rounded-full w-12 h-12 flex justify-center xxl:hidden'>
+          <Image src='icons/post.svg' width={24} height={24} alt='new post' />
+        </Link>
+        <Link href={'/'} className='hidden bg-white text-black font-bold rounded-full py-2 px-20 xxl:flex justify-center'>
+          Post
+        </Link>
+      </div>
+
+      {/* USER */}
+      <div className='flex items-start justify-between mt-4'>
+        <div className='flex items-center gap-2'>
+          {/* AVATAR */}
+          <div className='w-10 h-10 rounded-full overflow-hidden'>
+            <Image src={'/general/avatar.png'} width={40} height={40} alt='avatar' />
+          </div>
+
+          {/* INFO */}
+          <div className='hidden xxl:flex flex-col'>
+            <span className='font-bold'>Reda</span>
+            <span className='text-sm text-textGray'>@jrreda</span>
+          </div>
+        </div>
+
+        <div className='hidden xxl:block cursor-pointer font-bold'>...</div>
+      </div>
     </div>
   )
 }
